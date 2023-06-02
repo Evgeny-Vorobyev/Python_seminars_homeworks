22d997fc342e2f54f8a292dd23335f853b5fe963
evgeny <vorobyev.ev@gmail.com>
2023-06-02 13:09:07 +0300

    Add Exercise 2

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 
+# Seminar 1. Exercise 2
+
+number = 843
+digit_sum = 0
+
+digit_sum = sum(int(digit) for digit in str(number))
+
+print("Sum of numbers: ", digit_sum)
 
 
 
-
-print ("Hello world")
