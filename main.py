c6646fa463f6e9794d25b2199f1d933971db752c
evgeny <vorobyev.ev@gmail.com>
2023-06-02 18:41:12 +0300

    Add Exercise 2

--- main.py
+++ main.py
@@ -9,4 +9,14 @@ digit_sum = sum(int(digit) for digit in str(number))
 print("Sum of numbers: ", digit_sum)
 
 
+# Seminar 1. Exercise 4
 
+S = int(input("Input total S: "))
+
+PS_count = S // 4
+
+K_count = 2 * (PS_count + PS_count)
+
+print("Petya done: ", PS_count)
+print("Sergey done: ", PS_count)
+print("Katya done: ", K_count)
